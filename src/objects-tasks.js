c978e594ea44ba42cc1dbe983897a1c116fbfbde
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2025-12-06 02:40:24 +0300

    feat: add solution for sell tickets task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -156,8 +156,22 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let cash = 0;
+
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === 25) {
+      cash += 25;
+    }
+
+    if (queue[i] - 25 > cash) {
+      return false;
+    }
+
+    cash += queue[i] - 25;
+  }
+
+  return true;
 }
 
 /**
