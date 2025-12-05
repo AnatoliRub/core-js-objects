59966a38d35bc7d173d5695b0798021c972cd5bb
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2025-12-06 02:39:06 +0300

    feat: add solution for make word task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -130,8 +130,16 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  return Object.entries(lettersObject)
+    .reduce((acc, entry) => {
+      const [key, order] = entry;
+      order.forEach((ind) => {
+        acc[ind] = key;
+      });
+      return acc;
+    }, [])
+    .join('');
 }
 
 /**
